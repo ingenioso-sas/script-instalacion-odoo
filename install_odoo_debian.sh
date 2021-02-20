818160b0cd30e82f0cd21eb8d8f3934e70df471c
anderson <andersonbuitron@unicauca.edu.co>
2021-02-20 14:15:20 +0000

    reparacion de errores en instalacion para debian

--- install_odoo_debian.sh
+++ install_odoo_debian.sh
@@ -5,7 +5,7 @@
 #   para cualquier version, de Sistema operativo y para odoo, pero podria no 
 #   cumplirse algunas dependencias).
 #
-# Authors: Henry Robert Muwanika, Anderso buitron Papamija
+# Authors: Henry Robert Muwanika, Anderson buitron Papamija
 #-------------------------------------------------------------------------------
 # Podria instalarse varias instancias de odoo de las misma version, en un 
 #     sistema operativo Debian, configurando diferente puertos para cada instancia,
@@ -46,7 +46,7 @@ WEBSITE_NAME="example.com"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 LONGPOLLING_PORT="8072"
 # Set to "True" to install certbot and have ssl enabled, "False" to use http
-ENABLE_SSL="True"
+ENABLE_SSL="False"
 # Provide Email to register ssl certificate
 ADMIN_EMAIL="odoo@example.com"
 # configuracion de servidor ssh
@@ -82,7 +82,7 @@ sudo apt update
 sudo apt autoremove -y
 
 echo -e "\n============= Instalar utilidades ================"
-sudo apt install -y vim  git build-essential wget
+sudo apt install -y vim  git build-essential wget fail2ban
 #### disable vim visual mode in debian Buster ####
 sudo echo "set mouse-=a" >> ~/.vimrc
 
@@ -99,8 +99,8 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 
 # Update the package lists:
 sudo apt-get update
+sudo apt install -y postgresql postgresql-contrib 
 
-sudo apt install -y postgresql postgresql-contrib
 sudo systemctl enable postgresql
 
 echo -e "\n=========== Creando el usuario ODOO en PostgreSQL ================="
@@ -110,22 +110,20 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 # Instalado Dependencias
 #--------------------------------------------------
 echo -e "\n=================== Installing Python 3 + pip3 ============================"
-sudo apt install python3 python3-pip python3-dev python3-pil python3-lxml python3-dateutil python3-venv python3-wheel \
-wget python3-setuptools libfreetype6-dev libpq-dev libxslt1-dev libxml2-dev libzip-dev libldap2-dev libsasl2-dev libxslt1-dev node-less gdebi \
-zlib1g-dev libtiff5-dev libjpeg62-turbo-dev libopenjp2-7-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev fail2ban libssl-dev \
-libjpeg-dev libblas-dev libatlas-base-dev -y
-
-sudo -H pip3 install --upgrade pip
-pip3 install Babel decorator docutils ebaysdk feedparser gevent greenlet html2text Jinja2 lxml Mako MarkupSafe mock num2words ofxparse \
-passlib Pillow psutil psycogreen psycopg2 pydot pyparsing PyPDF2 pyserial python-dateutil python-openid pytz pyusb PyYAML qrcode reportlab \
-requests six suds-jurko vatnumber vobject Werkzeug XlsxWriter xlwt xlrd polib
+sudo apt install python3 python3-pip python3-dev python3-dev python3-venv \
+    python3-wheel libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev \
+    libtiff5-dev libjpeg62-turbo-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev \
+    liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev PyPDF2
+sudo apt -f install -y
 
 echo -e "\n================== Install python packages/requirements ============================"
-wget https://raw.githubusercontent.com/odoo/odoo/${OE_VERSION}/requirements.txt
-sudo pip3 install -r requirements.txt
+sudo pip3 install setuptools wheel
+sudo pip3 install -r https://raw.githubusercontent.com/odoo/odoo/${OE_VERSION}/requirements.txt
 
 echo -e "\n=========== Installing nodeJS NPM and rtlcss for LTR support =================="
 sudo apt install nodejs npm -y
+sudo apt -f install -y
+
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g rtlcss less less-plugin-clean-css
 
@@ -141,9 +139,7 @@ sudo npm install -g rtlcss less less-plugin-clean-css
 
 if [ $INSTALL_WKHTMLTOPDF = "True" ]; then
   echo -e "\n---- Install wkhtml and place shortcuts on correct place for ODOO 13 ----"
-
-  sudo apt install software-properties-common -y
-  sudo apt install xfonts-75dpi -y
+  sudo apt install software-properties-common xfonts-75dpi -y
 
   #pick up correct one from x64 & x32 versions:
   if [ "`getconf LONG_BIT`" == "64" ];then
@@ -172,10 +168,10 @@ else
 fi
 
 echo -e "\n=========== Creando directorio raiz ====================="
-if [ ! -d "$OE_ROOT" ] ; then
-  sudo mkdir $OE_ROOT
+if [ ! -d "$OE_HOME" ] ; then
+  sudo mkdir -p $OE_HOME
 fi
-sudo chmod -R 775 $OE_ROOT
+sudo chmod -R 775 $OE_HOME
 
 echo -e "\n======== Create ODOO system user =========="
 sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
@@ -189,7 +185,7 @@ fi
 sudo chmod -R 775 $OE_ODOO_CONF
 
 echo -e "\n======= Setting permissions on home folder ============="
-sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
+sudo chown -R $OE_USER:$OE_USER $OE_HOME
 
 sudo mkdir /var/log/$OE_USER
 sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
@@ -198,8 +194,8 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install ODOO
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
-# sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
-sudo git clone ./odoo-server/.git $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
+# sudo git clone ./odoo-server/.git $OE_HOME_EXT/
 
 if [ $IS_ENTERPRISE = "True" ]; then
     # Odoo Enterprise install!
@@ -254,6 +250,44 @@ if [ $IS_ENTERPRISE = "True" ]; then
 else
     sudo su root -c "printf 'addons_path=${OE_HOME_EXT}/addons,${OE_HOME}/custom/addons\n' >> ${OE_CONFIG_DIR}"
 fi
+
+sudo cat <<EOF >> ${OE_CONFIG_DIR}
+# configuracion adicional
+dbfilter = ^%d*
+limit_memory_hard = 1684354560
+limit_memory_soft = 1147483648
+limit_request = 8192
+limit_time_cpu = 60
+limit_time_real = 120
+limit_time_real_cron = -1
+list_db = False
+log_db = False
+log_db_level = warning
+log_handler = :INFO
+log_level = info
+max_cron_threads = 1
+osv_memory_age_limit = 1.0
+osv_memory_count_limit = False
+reportgz = False
+screencasts = None
+syslog = False
+test_enable = False
+test_file = False
+test_tags = None
+translate_modules = ['all']
+unaccent = False
+upgrades_paths =
+without_demo = False
+workers = 2
+smtp_password = xxxxxx
+smtp_port = 587
+smtp_server = smtp.gmail.com
+smtp_ssl = True
+smtp_user = cualquiera@gmail.com
+longpolling_port = $LONGPOLLING_PORT
+
+EOF
+
 sudo chown $OE_USER:$OE_USER ${OE_CONFIG_DIR}
 sudo chmod 640 ${OE_CONFIG_DIR}
 
@@ -302,11 +336,12 @@ sudo systemctl start odoo_$OE_USER.service
 #--------------------------------------------------
 if [ $INSTALL_NGINX = "True" ]; then
   echo -e "\n======== Installing and setting up Nginx ========="
+  sudo apt -f install -y
   sudo apt install -y nginx
   sudo systemctl enable nginx
   sudo systemctl start nginx
   
-  cat <<EOF > ~/odoo_nginx
+  cat <<EOF > ~/$WEBSITE_NAME
   
     #odoo server
     upstream odoo {
@@ -317,76 +352,84 @@ if [ $INSTALL_NGINX = "True" ]; then
         server 127.0.0.1:$LONGPOLLING_PORT;
     }
 
-    # http to https redirection
     server {
         listen 80;
-        listen [::]:80;
-        server_name $WEBSITE_NAME;
-    
-        # Proxy settings
-        proxy_read_timeout 720s;
-        proxy_connect_timeout 720s;
-        proxy_send_timeout 720s;
 
-        #   increase    proxy   buffer  size
-        proxy_buffers   16  64k;
-        proxy_buffer_size   128k;
+        # set proper server name after domain set
+        server_name $WEBSITE_NAME;
 
         # Add Headers for odoo proxy mode
         proxy_set_header X-Forwarded-Host \$host;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header X-Real-IP \$remote_addr;
+        add_header X-Frame-Options "SAMEORIGIN";
+        add_header X-XSS-Protection "1; mode=block";
+        proxy_set_header X-Client-IP \$remote_addr;
+        proxy_set_header HTTP_X_FORWARDED_HOST \$remote_addr;
 
-        # log
-        access_log /var/log/nginx/$OE_USER-access.log;
-        error_log /var/log/nginx/$OE_USER-error.log;
+        #   odoo    log files
+        access_log  /var/log/nginx/$OE_USER-access.log;
+        error_log       /var/log/nginx/$OE_USER-error.log;
+
+        #   increase    proxy   buffer  size
+        proxy_buffers   16  64k;
+        proxy_buffer_size   128k;
+
+        proxy_read_timeout 900s;
+        proxy_connect_timeout 900s;
+        proxy_send_timeout 900s;
 
         #   force   timeouts    if  the backend dies
-        proxy_next_upstream error   timeout invalid_header  http_500 http_502 http_503;
+        proxy_next_upstream error   timeout invalid_header  http_500    http_502
+        http_503;
 
         types {
             text/less less;
             text/scss scss;
         }
 
-        # Request for root domain
+        #   enable  data    compression
+        gzip    on;
+        gzip_min_length 1100;
+        gzip_buffers    4   32k;
+        gzip_types  text/css text/less text/plain text/xml application/xml application/json application/javascript application/pdf image/jpeg image/png;
+        gzip_vary   on;
+        client_header_buffer_size 4k;
+        large_client_header_buffers 4 64k;
+        client_max_body_size 0;
+
         location / {
+            proxy_pass    http://odoo;
+            # by default, do not forward anything
             proxy_redirect off;
-            proxy_pass http://odoo;
         }
 
         location /longpolling {
             proxy_pass http://odoochat;
         }
-
         location ~* .(js|css|png|jpg|jpeg|gif|ico)$ {
             expires 2d;
             proxy_pass http://odoo;
             add_header Cache-Control "public, no-transform";
         }
-
-        # Cache static files.
-        location ~* /[0-9a-zA-Z_]*/static/ {
-            proxy_cache_valid 200 90m;
+        # cache some static data in memory for 60mins.
+        location ~ /[a-zA-Z0-9_-]*/static/ {
+            proxy_cache_valid 200 302 60m;
             proxy_cache_valid 404      1m;
-            proxy_buffering on;
+            proxy_buffering    on;
             expires 864000;
-            proxy_pass http://odoo;
+            proxy_pass    http://odoo;
         }
-
-        # Gzip Compression
-        gzip_types text/css text/scss text/plain text/xml application/xml application/json application/javascript;
-        gzip on;
     }
 EOF
 
-  sudo mv ~/odoo_nginx /etc/nginx/sites-available/odoo
-  sudo ln -s /etc/nginx/sites-available/odoo /etc/nginx/sites-enabled/odoo
+  sudo mv ~/$WEBSITE_NAME /etc/nginx/sites-available/$WEBSITE_NAME
+  sudo ln -s /etc/nginx/sites-available/$WEBSITE_NAME /etc/nginx/sites-enabled/
   sudo rm /etc/nginx/sites-enabled/default
   sudo systemctl reload nginx
   sudo su root -c "printf 'proxy_mode = True\n' >> ${OE_CONFIG_DIR}"
-  echo "Done! The Nginx server is up and running. Configuration can be found at /etc/nginx/sites-available/odoo"
+  echo "Done! The Nginx server is up and running. Configuration can be found at /etc/nginx/sites-available/$WEBSITE_NAME"
 else
   echo "Nginx isn't installed due to choice of the user!"
 fi
